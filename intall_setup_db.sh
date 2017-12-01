88bdc84612ba07917652d74e7bada78c7c426fff
Ulises Olivares <uolivares@gdl.cinvestav.mx>
2017-11-30 20:19:27 -0600

    Create DB

--- intall_setup_db.sh
+++ intall_setup_db.sh
@@ -5,3 +5,6 @@ yum install postgresql postgresql-server postgresql-contrib
 
 # Create a user galaxy in the DB
 sudo -Hu postgres createuser galaxy
+
+# Create the DB called galaxy
+sudo -Hu postgres createdb -O galaxy galaxy
